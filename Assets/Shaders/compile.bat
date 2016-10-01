07258c793aeb71f39685b72ed0404d4ee4c66d5e
Ichi Jou <ichijou8282@gmail.com>
2016-10-02 03:23:30 +0800

    new shaders

--- Assets/Shaders/compile.bat
+++ Assets/Shaders/compile.bat
@@ -1,4 +1,6 @@
 D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V vertex.vert -o vertex.spv
 D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V fragment.frag -o fragment.spv
+D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V simplevert.vert -o simplevert.spv
+D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V simplefrag.frag -o simplefrag.spv
 
 pause
