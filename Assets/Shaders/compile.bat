548a186564e3d22652183dc8666c1956b05749d6
Ichi Jou <ichijou8282@gmail.com>
2016-10-05 11:29:06 +0800

    adjusted

--- Assets/Shaders/compile.bat
+++ Assets/Shaders/compile.bat
@@ -1,6 +1,8 @@
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V vertex.vert -o vertex.spv
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V fragment.frag -o fragment.spv
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V simplevert.vert -o simplevert.spv
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V simplefrag.frag -o simplefrag.spv
+D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V mrt.vert -o mrt_vert.spv
+D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V mrt.frag -o mrt_frag.spv
 
+D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V screenquad.vert -o screenquad_vert.spv
+
+D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V dirlightpass.frag -o dirlightpass_frag.spv
+D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V finalpass.frag -o finalpass_frag.spv
 pause
