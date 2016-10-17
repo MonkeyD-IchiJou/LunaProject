4289b5a6eaf052638a8efd967b852dadfbefd744
MonkeyD-IchiJou <ichijou8282@gmail.com>
2016-10-18 02:15:00 +0800

    compute shader included

--- Assets/Shaders/compile.bat
+++ Assets/Shaders/compile.bat
@@ -1,11 +1,13 @@
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V deferred.vert -o deferred_vert.spv
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V deferred.frag -o deferred_frag.spv
+D:/VulkanSDK/1.0.30.0/Bin/glslangValidator.exe -V deferred.vert -o deferred_vert.spv
+D:/VulkanSDK/1.0.30.0/Bin/glslangValidator.exe -V deferred.frag -o deferred_frag.spv
 
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V screenquad.vert -o screenquad_vert.spv
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V dirlightsubpass.frag -o dirlightsubpass_frag.spv
+D:/VulkanSDK/1.0.30.0/Bin/glslangValidator.exe -V screenquad.vert -o screenquad_vert.spv
+D:/VulkanSDK/1.0.30.0/Bin/glslangValidator.exe -V dirlightsubpass.frag -o dirlightsubpass_frag.spv
 
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V finalpass.frag -o finalpass_frag.spv
+D:/VulkanSDK/1.0.30.0/Bin/glslangValidator.exe -V finalpass.frag -o finalpass_frag.spv
 
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V text.vert -o text_vert.spv
-D:/VulkanSDK/1.0.26.0/Bin/glslangValidator.exe -V text.frag -o text_frag.spv
+D:/VulkanSDK/1.0.30.0/Bin/glslangValidator.exe -V text.vert -o text_vert.spv
+D:/VulkanSDK/1.0.30.0/Bin/glslangValidator.exe -V text.frag -o text_frag.spv
+
+D:/VulkanSDK/1.0.30.0/Bin/glslangValidator.exe -V compute.comp -o compute_comp.spv
 pause
--- Assets/Shaders/deferred.frag
+++ Assets/Shaders/deferred.frag
@@ -2,6 +2,8 @@
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_ARB_shading_language_420pack : enable
 
+layout(early_fragment_tests) in;
+
 // textures sampler
 layout (binding = 2) uniform sampler2D samplerColor;
 
