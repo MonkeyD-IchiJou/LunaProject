aab049ef3d3ab037b5053281a8ab65a1bacb7d27
Ichi Jou <ichijou8282@gmail.com>
2016-10-27 02:53:12 +0800

    camera component supported

---
+++ Source/CameraComponent.cpp
@@ -0,0 +1,47 @@
+#include "CameraComponent.h"
+#include "WinNative.h"
+#include "Entity.h"
+#include "TransformationComponent.h"
+
+#define GLM_FORCE_RADIANS
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace luna
+{
+	CameraComponent::CameraComponent() : Component(CAMERA_CTYPE)
+	{
+	}
+
+	CameraComponent::~CameraComponent()
+	{
+	}
+
+	void CameraComponent::Update()
+	{
+		if (m_active)
+		{
+			auto position = m_owner->transformation->position;
+			m_view = glm::lookAt(position, target, up);
+
+			WinNative* win = WinNative::getInstance();
+			m_projection = glm::perspective(glm::radians(fovy), win->getWinSizeX() / static_cast<float>(win->getWinSizeY()), nearZ, farZ);
+			m_projection[1][1] *= -1.f;
+		}
+	}
+
+	void CameraComponent::Reset()
+	{
+		this->m_owner = nullptr;
+		this->m_active = false;
+
+		target = {};
+		up = {};
+
+		fovy = 45.f;
+		nearZ = 0.1f;
+		farZ = 10.f;
+
+		m_view = {};
+		m_projection = {};
+	}
+}
---
+++ Source/CameraComponent.h
@@ -0,0 +1,39 @@
+#ifndef CAMERA_COMPONENT_H
+#define CAMERA_COMPONENT_H
+
+#include "Component.h"
+#include <glm\glm.hpp>
+
+namespace luna
+{
+	class CameraComponent :
+		public Component
+	{
+	public:
+		CameraComponent();
+		virtual ~CameraComponent();
+
+		void Update() override;
+		void Reset() override;
+
+		auto GetView() const { return this->m_view; }
+		auto GetProjection() const { return this->m_projection; }
+
+	public:
+		glm::vec3 target = {};
+		glm::vec3 up = {0.f, 1.f, 0.f};
+		
+		float fovy = 45.f;
+		float nearZ = 0.1f;
+		float farZ = 10.f;
+
+		bool maincam = false;
+
+	private:
+		glm::mat4 m_view = {};
+		glm::mat4 m_projection = {};
+	};
+}
+
+#endif
+
